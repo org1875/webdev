0122d533ff7cd77a8e9055b8297f040608cad1c0
karthik murali <karthikmurali123@gmail.com>
2025-01-13 21:48:38 +0530

    feature_1-commit1
    x
    
    Update package-lock.jsonmmmasterrr
    
    Update App.csscsssss
    
    x
    
    f1
    
    sss

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ const App = ()=>{
         
            <div className="bg" >
             <div>
-            <h1>Memories</h1>
+            <h1>Memories feature 1</h1>
             </div>
             
             
