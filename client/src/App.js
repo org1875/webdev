e25522369b39a3f659b5a3914ba7d790122b42c6
karthik murali <karthikmurali123@gmail.com>
2025-01-13 21:44:43 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+//
+//import memories from './images/267434.jpg';
+import './App.css'
+const App = ()=>{
+    return(
+        
+           <div className="bg" >
+            <div>
+            <h1>Memories</h1>
+            </div>
+            
+            
+           </div>     
+                
+            
+
+        
+    )
+}
+
+export default App;
